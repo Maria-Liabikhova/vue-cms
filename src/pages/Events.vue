2adbf30747ed2b4674c71ff2de2afb3de1e75282
unknown <mashagi@yandex.ru>
2020-11-19 21:06:28 +0300

    add: Events page completed

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -3,12 +3,39 @@
     <h1>Events</h1>
     <hr>
     <hr>
-    <router-link to="/">Home Page</router-link>
+    <h2 @mouseover="onHover" @mouseout="noHover"> Counter: {{ count }}</h2>
+    <!-- <button class="events-btn" @click="count++">+1</button> -->
+    <button class="events-btn" @click="addCount">+1</button>
+    <button class="events-btn" @click="count--">-1</button>
+
+    <br><router-link to="/">Home Page</router-link>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Events',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    addCount: function() {
+      this.count++
+    },
+    onHover: function() {
+      event.target.style.color = "red"
+    },
+    noHover: function() {
+      event.target.style.color = "blue"
+    }
+  }
 }
 </script>
+
+<style scoped>
+.events-btn {
+  margin-bottom: 40px;
+}
+</style>
