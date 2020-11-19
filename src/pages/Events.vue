48c925e9c6ba0f0c34fda5db208cea03d2a007ca
unknown <mashagi@yandex.ru>
2020-11-19 23:02:50 +0300

    add: passing parameter to the methods

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -7,6 +7,9 @@
     <!-- <button class="events-btn" @click="count++">+1</button> -->
     <button class="events-btn" @click="addCount">+1</button>
     <button class="events-btn" @click="count--">-1</button>
+    <h2> {{ title }}<br> Result:{{ countSecond }}</h2>
+    <button class="events-btn events-btn--long" @click="addCountSecond(5, 'Changed on 5')">+5</button>
+    <button class="events-btn events-btn--long" @click="addCountSecond(10, 'Changed on 10')">+10</button>
 
     <br><router-link to="/">Home Page</router-link>
   </div>
@@ -17,12 +20,23 @@ export default {
   name: 'Events',
   data() {
     return {
-      count: 0
+      count: 0,
+      countSecond: 0,
+      title: 'Count:'
     }
   },
   methods: {
     addCount: function() {
       this.count++
+    },
+    addCountSecond: function(num, str) {
+      this.countSecond+=num
+      this.title = str
+      if (num === 5) {
+        event.target.style.color = 'blue'
+      } else if (num === 10) {
+        event.target.style.color = 'red'
+      }
     },
     onHover: function() {
       event.target.style.color = "red"
@@ -38,4 +52,7 @@ export default {
 .events-btn {
   margin-bottom: 40px;
 }
+.events-btn--long {
+  width: 70px
+  }
 </style>
