5affd24aab1900c62dd087d2aab0a7d348da9b17
unknown <mashagi@yandex.ru>
2020-11-23 21:05:36 +0300

    Parameters and arguments in directives

--- src/pages/Directives.vue
+++ src/pages/Directives.vue
@@ -3,7 +3,8 @@
     <h1>Directives</h1>
     <hr>
     <hr>
-    <h2 v-colored>{{ title}}</h2>
+    <h2 v-colored:background="'purple'">{{ title}}</h2>
+    <h2 v-colored:color="'orange'">{{ title}}</h2>
     <br><router-link class="home-link" to="/">Home Page</router-link>
   </div>
 </template>
